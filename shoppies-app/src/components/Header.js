377cb96c4cdc60999f20709b052119b3adf8fb4e
Sam O <sam.orend22@gmail.com>
2021-04-27 14:19:17 -0400

    boiler plate clean-up, installed react-bootstrap

---
+++ shoppies-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+export function Header(props) {
+    return (
+        <div className="header-wrapper">
+            <h1>The Shoppies</h1>
+        </div>
+    );
+}
+
+export default Header;
