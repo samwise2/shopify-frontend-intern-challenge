b42f18b3869dac0fdd82a82cfa5c8a72be275389
Sam O <sam.orend22@gmail.com>
2021-04-27 14:36:25 -0400

    Added header

--- shoppies-app/src/App.js
+++ shoppies-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
--- shoppies-app/src/components/Header/Header.js
+++ shoppies-app/src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import './Header.css';
 
 export function Header(props) {
     return (
         <div className="header-wrapper">
-            <h1>The Shoppies</h1>
+            <h1 className="header-text">The Shoppies</h1>
         </div>
     );
 }
